8d9556d62265b9f05840254feb28180bc01a1731
username29800 <piapia78490@gmail.com>
2026-08-07 13:53:31 +0900

    install noto-cjk fonts by default

--- redcat/env/testing.sh
+++ redcat/env/testing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # run as root
-dnf install -y openbox fluxbox xterm rxvt-unicode ibus ibus-hangul ibus-gtk* google-noto-cjk-fonts-common google-noto-emoji-*
+dnf install -y openbox fluxbox xterm rxvt-unicode ibus ibus-hangul ibus-gtk* google-noto-*-cjk-ttc-fonts google-noto-cjk-fonts-common google-noto-emoji-*
