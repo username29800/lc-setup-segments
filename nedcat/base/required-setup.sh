90d676d5ee16e1a928828b995893e35655f88239
username29800 <piapia78490@gmail.com>
2026-09-01 19:11:10 +0900

    ensure home directory creation on useradd

--- nedcat/base/required-setup.sh
+++ nedcat/base/required-setup.sh
@@ -6,4 +6,4 @@
 sed -i 's,^# en_US.UTF-8 UTF-8$,en_US.UTF-8 UTF-8,' /etc/locale.gen
 locale-gen
 # 2. adduser
-useradd -G sudo -s /usr/bin/bash -p "`openssl passwd -6 'changemepasswd'`" user
+useradd -mG sudo -s /usr/bin/bash -p "`openssl passwd -6 'changemepasswd'`" user
