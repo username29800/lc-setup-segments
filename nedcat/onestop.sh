ba227026e6bcc6b1b65908b1be71e251e6cd031b
username29800 <piapia78490@gmail.com>
2026-09-07 03:24:58 +0900

    add user config section to onestop.sh

--- nedcat/onestop.sh
+++ nedcat/onestop.sh
@@ -1,4 +1,5 @@
 # run as root
+# usage: sh ./onestop.sh [user] [home dir]
 
 apt update && apt upgrade -y
 apt install -y git
@@ -25,6 +26,17 @@ sh server/default-setup.sh
 sh server/sysadmin.sh
 sh server/sysadmin-setup.sh
 
+# segment: app platform
+sh apps/platform.sh
+
 # pseudo-segment: graphical environment
 apt install -y xfce4 dbus-x11
+
+# pseudo-segment: user
+cp -a . "$2/scripts_setup"
+chown -R "$1" "$2/scripts_setup"
+sudo -u "$1" "cd $2/scripts_setup/nedcat
+&& sh base/advanced-setup-u.sh
+&& sh apps/platform-user.sh
+&& sh apps/internet-setup.sh $2"
 
