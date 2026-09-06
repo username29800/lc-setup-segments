7f9bcbdd840590b7f2396cc56b59a2ece0b57148
username29800 <piapia78490@gmail.com>
2026-09-07 03:33:07 +0900

    add user config: fix sudo argument

--- nedcat/onestop.sh
+++ nedcat/onestop.sh
@@ -35,8 +35,8 @@ apt install -y xfce4 dbus-x11
 # pseudo-segment: user
 cp -a . "$2/scripts_setup"
 chown -R "$1" "$2/scripts_setup"
-sudo -u "$1" cd $2/scripts_setup/nedcat \
+sudo -u "$1" sh -c "$2/scripts_setup/nedcat \
 && sh base/advanced-setup-u.sh  \
 && sh apps/platform-user.sh \
-&& sh apps/internet-setup.sh $2
+&& sh apps/internet-setup.sh $2"
 
