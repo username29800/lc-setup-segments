c539fdb61a54482aeb48da3222f49b7d4562f139
username29800 <piapia78490@gmail.com>
2026-09-07 17:17:30 +0900

    add one-stop setup script to redcat

---
+++ redcat/onestop.sh
@@ -0,0 +1,42 @@
+# run as root
+# usage: sh ./onestop.sh [user] [home dir]
+
+dnf update && dnf upgrade -y
+dnf install -y git
+
+# segment: base
+sh base/required.sh
+#sh base/required-setup.sh
+sh base/advanced.sh
+sh base/advanced-setup-u.sh
+
+# segment: env-buildtools
+sh env/build-base.sh
+sh env/build-c.sh
+sh env/build-sh.sh
+
+# segment: env-base
+sh env/remote.sh
+sh env/font-extras.sh
+sh env/testing.sh
+
+# segment: server
+sh server/default.sh
+sh server/default-setup.sh
+sh server/sysadmin.sh
+sh server/sysadmin-setup.sh
+
+# segment: app platform
+#sh apps/platform.sh
+
+# pseudo-segment: graphical environment
+dnf install -y xfce4 dbus-x11
+
+# pseudo-segment: user
+cp -a . "$2/scripts_setup"
+chown -R "$1" "$2/scripts_setup"
+sudo -u "$1" sh -c "cd $2/scripts_setup/ \
+&& sh base/advanced-setup-u.sh  \
+&& sh apps/platform-user.sh \
+&& sh apps/internet-setup.sh $2"
+
